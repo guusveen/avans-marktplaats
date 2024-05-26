d60bd2a760e7f47fa5a339a31eda92a7cf14b37e
Guus Veen <guus_veen@outlook.com>
2024-05-26 23:13:33 +0200

    remove ignored files and update .gitignore

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 // Register a new user
@@ -27,41 +26,6 @@ router.post('/register', (req, res) => {
         });
       });
     }
-  });
-});
-
-// Login user and return JWT
-router.post('/login', (req, res) => {
-  const { email, password } = req.body;
-
-  // Find user by email
-  User.findOne({ email }).then(user => {
-    if (!user) {
-      return res.status(404).json({ email: 'User not found' });
-    }
-
-    // Check password
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        // User matched
-        const payload = { id: user.id, username: user.username }; // Create JWT payload
-
-        // Sign token
-        jwt.sign(
-          payload,
-          'secret', // secret key
-          { expiresIn: 3600 }, // 1 hour in seconds
-          (err, token) => {
-            res.json({
-              success: true,
-              token: 'Bearer ' + token
-            });
-          }
-        );
-      } else {
-        return res.status(400).json({ password: 'Password incorrect' });
-      }
-    });
   });
 });
 
