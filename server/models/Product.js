c6aae91d20b790c3867987163327ce6853bbd26d
SpicyDavid <61142315+SpicyDavid@users.noreply.github.com>
2024-06-06 15:47:12 +0200

    Link tussen Product en user via email

--- server/models/Product.js
+++ server/models/Product.js
@@ -6,7 +6,24 @@ const ProductSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String },
   price: { type: Number, required: true },
-  user: { type: Schema.Types.ObjectId, ref: 'User' }
+  userEmail: { type: String, required: true }
 });
+
+ProductSchema.pre('save', function (next) {
+  const product = this;
+  mongoose.model('User').findOne({ email: product.userEmail }, function (err, user) {
+    if (err) {
+      return next(err);
+    } else if (!user) {
+      return next(new Error('Gebruiker met het opgegeven e-mailadres bestaat niet'));
+    }
+    next();
+  });
+});
+
+const User = mongoose.model('User', UserSchema);
+const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = { User, Product };
 
 module.exports = mongoose.model('Product', ProductSchema);
